eb012bca801f136042a7d2efebd2f1ab3223f67d
CMarzin <corentinmarzin@Gmail.com>
2017-11-27 12:03:47 +0100

    [ADD] final part tuto getting started

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
+  def index
+    @articles = Article.all
+  end
+
   def index
     @articles = Article.all
   end
@@ -9,6 +16,27 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
   end
 
   def create
